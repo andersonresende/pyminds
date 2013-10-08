dcd04b9cdbe5c75b65018f5f39cd665f71d66f7d
Anderson <anderson@anderson-dt.(none)>
2013-10-08 09:39:25 -0300

    modificando css

--- pyminds/urls.py
+++ pyminds/urls.py
@@ -4,14 +4,14 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'uol.views.home', name='home'),
-    # url(r'^uol/', include('uol.foo.urls')),
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'uol.views.home', name='home'),
+#     # url(r'^uol/', include('uol.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+#     # Uncomment the admin/doc line below to enable admin documentation:
+#     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+#     # Uncomment the next line to enable the admin:
+#     (r'^admin/', include(admin.site.urls)),
+# )
--- review/models.py
+++ review/models.py
@@ -5,5 +5,3 @@ from django.db import models
 class Question(models.Model):
     text = models.CharField(max_length=140, verbose_name="")
     date = models.DateTimeField(default=datetime.datetime.today())
-
-
