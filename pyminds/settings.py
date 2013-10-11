4f346db0db3984f6e0ba1ed965a5ba4d93e643fa
Anderson <anderson@anderson-dt.(none)>
2013-10-11 16:17:04 -0300

    criando revisoes e agendamentos

--- pyminds/settings.py
+++ pyminds/settings.py
@@ -160,21 +160,21 @@ LOGGING = {
 
 ### Heroku ###
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
+# # Allow all host headers
+# ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+# # Static asset configuration
+# import os
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
--- review/models.py
+++ review/models.py
@@ -2,6 +2,25 @@ import datetime
 from django.db import models
 
 
+class Review(models.Model):
+	pass
+
+
+class Schedule(models.Model):
+	checked = models.BooleanField(default=False)
+	rate = models.IntegerField(null=True, blank=True)
+	date = models.DateField()
+	review = models.ForeignKey(Review)
+
+
 class Question(models.Model):
     text = models.CharField(max_length=140, verbose_name="")
     date = models.DateTimeField(default=datetime.datetime.today())
+    review = models.ForeignKey(Review)
+
+
+
+
+
+
+
