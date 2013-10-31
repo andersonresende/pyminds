b2256c9f754783f75dfd7294f06a3a1a2df98392
Anderson <anderson@anderson-desk.(none)>
2013-10-31 09:17:37 -0300

    novo deploy

--- pyminds/settings.py
+++ pyminds/settings.py
@@ -159,7 +159,7 @@ LOGGING = {
 }
 
 ### Heroku ###
-Parse database configuration from $DATABASE_URL
+#Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
