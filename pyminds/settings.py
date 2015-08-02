23c4cdb2bacc14808356c32dd2f02815fbc97be0
Anderson Resende <andersonresende86@gmail.com>
2015-08-02 16:23:34 -0300

    Added django-widget-tweaks

--- pyminds/settings.py
+++ pyminds/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'review',
     'south',
+    'widget_tweaks',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
