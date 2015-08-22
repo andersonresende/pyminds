b3ab693d2f89dcabee843b7db087b3ed7fa6e30b
Anderson Resende <andersonresende86@gmail.com>
2015-08-22 12:26:23 -0300

    Changed settings database to postgres

--- pyminds/local_settings.py
+++ pyminds/local_settings.py
@@ -2,12 +2,12 @@ from settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pyminds.db',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pyminds',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'me',
-        'PASSWORD': '883',
+        'USER': 'local_user',
+        'PASSWORD': '123',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
