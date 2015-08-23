d764d2ef6e24ed954ae609ccb10eea87e50fce5e
Anderson Resende <andersonresende86@gmail.com>
2015-08-23 20:40:19 -0300

    Clean code

--- review/urls.py
+++ review/urls.py
@@ -1,12 +1,13 @@
+#! coding: utf-8
+
 from django.conf.urls import patterns, include, url
-from django.conf import settings
+
 from .views import *
 
-
 urlpatterns = patterns(
-    '',
-   url (r'^$', application_home,name='home'),
+   '',
+   url(r'^$', application_home, name='home'),
    url(r'^schedule/(.+)/$', schedule_page, name='schedule_page'),
    url(r'^questions/$', questions, name='questions'),
-    url(r'^closed_questions/$', closed_questions, name='closed_questions'),
+   url(r'^closed_questions/$', closed_questions, name='closed_questions'),
 )
