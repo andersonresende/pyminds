b47e8095b4e5f3e2fe694138d968e68182a40adb
Anderson Resende <andersonresende86@gmail.com>
2015-11-29 21:27:39 -0300

    Update local_settings

--- pyminds/local_settings.py
+++ pyminds/local_settings.py
@@ -3,11 +3,10 @@ from settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pyminds',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'local_user',
+        'NAME': 'pyminds',
+        'USER': 'pyminds_user',
         'PASSWORD': '123',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
