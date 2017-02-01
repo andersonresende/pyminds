c3a61193b07a4f9e0bd1831969418b1392e5fc4b
Anderson Resende <andersonresende86@gmail.com>
2017-02-01 11:16:41 -0300

    Fixed storage

--- pyminds/local_settings.py
+++ pyminds/local_settings.py
@@ -1,12 +1,5 @@
 from settings import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pyminds',
-        'USER': 'pyminds_user',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
--- review/urls.py
+++ review/urls.py
@@ -1,16 +1,15 @@
 #! coding: utf-8
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
 
 from .views import *
 
-urlpatterns = patterns(
-   '',
+urlpatterns = [
    url(r'^$', application_home, name='home'),
    url(r'^schedule/(.+)/$', schedule_page, name='schedule_page'),
    url(r'^questions/$', questions, name='questions'),
    url(r'^closed_questions/$', closed_questions, name='closed_questions'),
    url(r'^question/forgot/$', ForgotQuestionView.as_view(),
        name='forgot_question')
-)
+]
